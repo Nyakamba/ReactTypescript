bddd5cac79ceb93148945d8f9a8304a38bcfd64a
Nyakamba <omwegaenock@gmail.com>
2023-12-23 16:24:32 +0300

    props type

--- react-typescript-demo/src/components/Greet.tsx
+++ react-typescript-demo/src/components/Greet.tsx
@@ -1,7 +1,18 @@
-export const Greet = () => {
+type GreetProps = {
+  name: string;
+  messageCount?: number;
+  isLoggedIn: boolean;
+};
+
+export const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props;
   return (
     <div>
-      <h2>Welcome Enock! You have 10 unread messages</h2>
+      <h2>
+        {props.isLoggedIn
+          ? `Welcome ${props.name}! You have ${messageCount} unread messages`
+          : "Welcome Guest"}
+      </h2>
     </div>
   );
 };
