d1e6b365263c9eb693ebdb135500483a0ecd2e0e
Nyakamba <omwegaenock@gmail.com>
2023-12-23 19:51:33 +0300

    event typing

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import { Heading } from "./Heading";
 import { Oscar } from "./Oscar";
+import { Button } from "./components/Button";
 import { Greet } from "./components/Greet";
+import { Input } from "./components/Input";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
 import { Status } from "./components/Status";
@@ -22,12 +24,23 @@ function App() {
       {/* <Greet name="Enock" messageCount={10} isLoggedIn={false} />
       <Person name={personName} />
       <PersonList names={nameList} /> */}
-      <Status status="loading" />
+      {/* <Status status="loading" />
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading> Oscar goes to Leandro Dicpario</Heading>
       </Oscar>
-      <Greet name="Enock" isLoggedIn={true} />
+      <Greet name="Enock" isLoggedIn={true} /> */}
+      {/* <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event, id);
+        }}
+      /> */}
+      <Input
+        value=""
+        handleChange={(event) => {
+          console.log(event);
+        }}
+      />
     </div>
   );
 }
