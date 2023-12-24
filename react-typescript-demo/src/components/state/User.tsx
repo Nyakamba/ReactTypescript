81deef2a962def2e82f057e4b86ad2d608ef6e6c
Nyakamba <omwegaenock@gmail.com>
2023-12-24 20:14:40 +0300

    usereducer hook with typescript

---
+++ react-typescript-demo/src/components/state/User.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+type AuthUser = {
+  name: string;
+  email: string;
+};
+
+export const User = () => {
+  const [user, setUser] = useState<AuthUser | null>(null);
+  //This line used  for usesate assertion
+  //  const [user, setUser] = useState<AuthUser >({} as AuthUser);
+
+  const handleLogin = () => {
+    setUser({
+      name: "Enock",
+      email: "omwegaenock@gmail.com",
+    });
+  };
+
+  //usestate assertion
+  //   const handleLogout = () => {
+  //     setUser(null);
+  //   };
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      {/* <button onClick={handleLogout}>Logout</button> */}
+      <div>User name is {user?.name}</div>
+      <div>User email is {user?.email} </div>
+    </div>
+  );
+};
